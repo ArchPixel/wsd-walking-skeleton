9856d8e95cb22456a50eac5ab07d2a0eb2faf3ea
xudong <xudong_yan@hotmail.com>
2023-01-16 19:24:08 +0200

    commit test

---
+++ flyway/sql/V2__test_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Journeys (
+  Departure TIMESTAMP NOT NULL,
+  Arrival TIMESTAMP NOT NULL,
+  Departure_station_ID SERIAL NOT NULL,
+  Departure_station_name VARCHAR(255) NOT NULL,
+  Target_station_ID SERIAL NOT NULL,
+  Target_station_name VARCHAR(255) NOT NULL,
+  Travel_length_m SERIAL NOT NULL,
+  Duration_sec SERIAL NOT NULL
+);
